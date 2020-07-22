5e51b1267e006dd160ddc43764252ba3cfb87f8b
Davuluri Hemanth Chowdary <hemanthdavuluri98@gmail.com>
2020-07-22 15:29:53 +0530

    Form Code clean

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,42 +3,10 @@ import postRequest from "../api/postRequest";
 import "../css/Form.css";
 
 const Form = () => {
-  let [productName, setProductName] = useState("");
-  let [price, setPrice] = useState(0);
-
-  const submit = async () => {
-    //console.log(productName);
-    //console.log(price);
-    let postData = await postRequest
-      .post("/posts", {
-        productName: productName,
-        productPrice: price,
-      })
-      .then(function (response) {
-        console.log(response);
-      });
-  };
+ 
   return (
     <div className="card-container">
-      <div
-        
-        style={{ display: "grid", justifyContent: "center", padding: "45%" }}
-      >
-        <label>Enter the product name</label>
-        <input
-          type="text"
-          onChange={(e) => setProductName((productName = e.target.value))}
-        ></input>
-        <br />
-        <label>Enter the product Price</label>
-        <input
-          type="number"
-          value={price}
-          onChange={(e) => setPrice((price = e.target.value))}
-        ></input>
-        <br />
-        <button onClick={submit}>SUBMIT</button>
-      </div>
+      
     </div>
   );
 };
