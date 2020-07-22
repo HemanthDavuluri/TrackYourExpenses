ae85d71eb4b4b16cc400cba2af5abe3bb8665d0f
Davuluri Hemanth Chowdary <hemanthdavuluri98@gmail.com>
2020-07-22 06:42:45 +0530

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import postRequest from "../api/postRequest";
+
+const Form = () => {
+  let [productName, setProductName] = useState("");
+  let [price, setPrice] = useState(0);
+
+  const submit = async () => {
+    //console.log(productName);
+    //console.log(price);
+    let postData = await postRequest
+      .post("/posts", {
+        productName: productName,
+        productPrice: price,
+      })
+      .then(function (response) {
+        console.log(response);
+      });
+  };
+  return (
+    <div>
+      <div
+        style={{ display: "grid", justifyContent: "center", padding: "15%" }}
+      >
+        <label>Enter the product name</label>
+        <input
+          type="text"
+          onChange={(e) => setProductName((productName = e.target.value))}
+        ></input>
+        <br />
+        <label>Enter the product Price</label>
+        <input
+          type="number"
+          value={price}
+          onChange={(e) => setPrice((price = e.target.value))}
+        ></input>
+        <br />
+        <button onClick={submit}>SUBMIT</button>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
