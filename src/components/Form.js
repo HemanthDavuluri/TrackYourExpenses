29c468fe6da0e41241ffd9d257737345e98a8097
Davuluri Hemanth Chowdary <hemanthdavuluri98@gmail.com>
2020-07-23 15:11:00 +0530

    master

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,12 +5,29 @@ import "../css/Form.css";
 const Form = () => {
  
   return (
-    <div className="card-container">
-      <div className="card-content">
-       
-        <input type="text" placeholder="Purchased Item"></input>
-        
-        <input type="number" placeholder="Price"></input>
+    <div>
+      <div className="form">
+        <input
+          type="text"
+          name="PurchasedItem"
+          autoComplete="off"
+          required
+        ></input>
+        <label className="form-label" htmlFor="PurchasedItem">
+          <span className="form-span">Purchased Item</span>
+        </label>
+      </div>
+      <br/>
+      <div className="form">
+        <input
+          type="text"
+          name="ItemPrice"
+          autoComplete="off"
+          required
+        ></input>
+        <label className="form-label" htmlFor="ItemPrice">
+          <span className="form-span">Price</span>
+        </label>
       </div>
     </div>
   );
